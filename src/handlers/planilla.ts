fd773b1adcbbb4f3295880b898b38de688eb0cd3
Maximiliano Barrionuevo <maxisbarrionuevo@gmail.com>
2025-04-07 21:01:04 -0300

    se modifica getDatosObras

--- src/handlers/planilla.ts
+++ src/handlers/planilla.ts
@@ -42,22 +42,16 @@ export const createDatosObra = async (req: Request, res: Response) => {
 };
 
 export const getDatosObras = async (req: Request, res: Response) => {
-    const { clienteId } = req.params;
-    console.log("📥 ID del cliente recibido:", clienteId);
-
+    const { clienteId } = req.params
+    console.log(clienteId)
     try {
         const datosObras = await DatosObra.findAll({
-            where: { clienteId },
+            where: {clienteId},
             include: [{ model: VentanaCurvado }],
             order: [['entrega', 'DESC']],
         });
-
-        console.log("✅ Obras encontradas:", datosObras.length);
         res.status(200).json(datosObras);
-    } catch (error: any) {
-        console.error("❌ Error al obtener datos de obras:");
-        console.error("Mensaje:", error.message);
-        console.error("Stack:", error.stack);
+    } catch (error) {
         res.status(500).json({ error: 'Error al obtener datos de obras' });
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ const corsOptions: CorsOptions = {
         }
     },
     methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-    allowedHeaders: ['Content-Type, Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 

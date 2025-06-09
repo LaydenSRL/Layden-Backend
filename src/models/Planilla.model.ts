1022dae295d2effb205bef67450b2f065228e8e5
Maximiliano Barrionuevo <maxisbarrionuevo@gmail.com>
2025-06-09 11:32:53 -0300

    se puso text en vez de string

--- src/models/Planilla.model.ts
+++ src/models/Planilla.model.ts
@@ -116,7 +116,7 @@ class Obras extends Model {
   @Column({ type: DataType.STRING, allowNull: false })
   declare localidad: string;
 
-  @Column({ type: DataType.STRING, allowNull: true })
+  @Column({ type: DataType.TEXT, allowNull: true })
   declare observacionesObras: string;
 
   @HasMany(() => VentanaCurvado)

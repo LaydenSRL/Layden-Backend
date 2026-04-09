513b46177fb62911c272325b6d2fbaa9464f4f43
Maximiliano Barrionuevo <maxisbarrionuevo@gmail.com>
2026-04-09 18:04:51 -0300

    [add] insumos

--- src/models/Users.model.ts
+++ src/models/Users.model.ts
@@ -1,6 +1,43 @@
 import { Table, Column, Model, DataType, AllowNull, Default, HasMany, IsEmail } from 'sequelize-typescript';
 import Obras from './Planilla.model';
 
+type RendimientoTipo = "ml" | "unidad" | "tapadesague" | "esquinaRemate" | "mm" | "tornillo" | "tapatornillo" | "tornillosMosq";
+
+type Insumo = {
+  name: string;
+  rendimiento: number;
+  tipo: RendimientoTipo;
+};
+
+const rendimientoInsumos: Insumo[] = [
+  { name: "Tornillo de Colocacion", rendimiento: 500, tipo: "tornillo" },
+  { name: "Tapatornillo", rendimiento: 400, tipo: "tapatornillo" },
+  { name: "Espuma", rendimiento: 30000, tipo: "ml" },
+  { name: "Sellador", rendimiento: 6000, tipo: "ml" },
+  { name: "Esquina Remate Interior", rendimiento: 1, tipo: "esquinaRemate" },
+  { name: "Tornillo Cabezon", rendimiento: 400, tipo: "ml" },
+  { name: "Tornillos Punta Aguja", rendimiento: 600, tipo: "tornillosMosq" },
+  { name: "Tapa Desague", rendimiento: 2, tipo: "tapadesague" },
+];
+
+type ConfigCalculo = {
+  margen: number;
+  distancia: number;
+  porcentajeReciclado: number;
+};
+
+const configCalculo: ConfigCalculo = {
+  margen: 300,
+  distancia: 700,
+  porcentajeReciclado: 50,
+};
+
+const insumos = {
+  rendimientoInsumos,
+  configCalculo
+}
+
+
 @Table({ tableName: 'Users' })
 class User extends Model {
   @Column({ type: DataType.STRING, primaryKey: true })
@@ -16,7 +53,7 @@ class User extends Model {
   declare rol: string; // ejemplo: 'admin', 'vendedor', etc.
 
   @IsEmail
-  @Column({ type: DataType.STRING, allowNull: false})
+  @Column({ type: DataType.STRING, allowNull: false })
   declare email: string
 
   @Default(true)
@@ -34,6 +71,9 @@ class User extends Model {
   @AllowNull(true)
   @Column({ type: DataType.JSONB })
   declare vendedores: string[];
+
+  @Column({ type: DataType.JSONB, defaultValue: insumos })
+  declare insumos: { rendimientoInsumos: Insumo[]; configCalculo: ConfigCalculo; }
 }
 
 export default User;
